c80a1a9f970f88689ede7cafc53f9661ee11083d
Anna Turek <anutkakpi@ukr.net>
2024-11-02 15:47:13 +0200

    Done

--- src/components/travelList.jsx
+++ src/components/travelList.jsx
@@ -11,8 +11,23 @@ export const TravelList = () => {
       {TravelPlans.map((plan) => {
         console.log(plan);
         return (
-          <div key={plan.id}>
-            <h3>{plan.destination}</h3>
+          <div key={plan.id} id="plan-card">
+            <img src={plan.image} alt={plan.destination} />
+            <div className="inner-container">
+              <h3>
+                {plan.destination}({plan.days} days)
+              </h3>
+              <p>{plan.description}</p>
+              <h4>Price: {plan.totalCost}$</h4>
+              {plan.totalCost > 1500 ? <span id="premium">Premium</span> : null}
+              {plan.totalCost < 350 ? (
+                <span id="great-deal">Great Deal</span>
+              ) : null}
+              {plan.allInclusive ? (
+                <span id="included">All Inclusive</span>
+              ) : null}
+              <button onClick={handleDelete} Delete></button>
+            </div>
           </div>
         );
       })}
